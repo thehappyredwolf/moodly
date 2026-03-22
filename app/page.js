75968597e6c023f568101dcc0390c11b43f321b9
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-22 07:34:16 +0200

    Add the hero section to the main page

--- app/page.js
+++ app/page.js
@@ -1,9 +1,10 @@
+import Hero from "@/components/Hero";
 import { Main } from "next/document";
 
 export default function HomePage() {
   return (
       <Main className="">
-        
+        <Hero />
       </Main>
   );
 }
