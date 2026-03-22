17d1ab1372d5d62caa2a613830dd17163dd8735f
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-22 07:40:22 +0200

    Change how dashboard pages renders based on authentication

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,12 +1,27 @@
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
 export const metadata = {
   title: "Moodly | Dashboard",
 };
 
 export default function DashboardPage(){
+    const isAuthenticated = false;
+    const children = (
+        <Login />
+    );
+
+    if (isAuthenticated){
+        children = (
+            <Dashboard />
+        )
+    }
+
     return (
-        <div>
-            <h1>This is the dashboard</h1>
-        </div>
+        <Main>
+            {children}
+        </Main>
         
     )
 }
