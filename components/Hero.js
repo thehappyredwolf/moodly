95ccde312f74bde832a4c5d06f3f304e6ad70340
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-23 23:33:03 +0200

    Use the fugaz font inside the hero section

--- components/Hero.js
+++ components/Hero.js
@@ -1,9 +1,18 @@
+import { Fugaz_One } from 'next/font/google';
 import React from 'react'
+
+
+const fugazOne = Fugaz_One({
+  variable: "--font-fugaz-one",
+  subsets: ["latin"],
+  weight: ["400"]
+});
+
 
 export default function Hero() {
   return (
     <div className="py-10 sm:py-14 md:py-20">
-      <h1><span>Moodly</span> helps you track your <span>daily</span> mood!</h1>
+      <h1 className={`${fugazOne.className}`} ><span>Moodly</span> helps you track your <span>daily</span> mood!</h1>
     </div>
   )
 }
