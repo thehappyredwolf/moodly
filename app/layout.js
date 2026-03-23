0957c1218fb63d03eb198a1052400d321eb0f217
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-24 01:33:30 +0200

    Update the content of the footer

--- app/layout.js
+++ app/layout.js
@@ -34,7 +34,12 @@ export default function RootLayout({ children }) {
     </header>
   );
 
-  const footer = <footer className="p-4 sm:p-8"></footer>;
+<<<<<<< HEAD
+=======
+  const footer = <footer className="p-4 sm:p-8 grid place-items-center">
+    <p className={`text-indigo-500 ${fugazOne.className}`}>Created with ❤️ by Abdelrahman Elsayed</p>
+  </footer>;
+>>>>>>> 0b067f4 (Initial commit)
 
   return (
     <html lang="en" suppressHydrationWarning>
