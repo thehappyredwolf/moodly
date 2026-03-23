aa2b6522ec42989f0d4e795b1230bc93376abac4
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-23 23:29:27 +0200

    Add padding to the sections and an h1 to the hero

--- app/layout.js
+++ app/layout.js
@@ -32,7 +32,7 @@ export default function RootLayout({ children }) {
   );
 
   const footer = (
-    <footer>
+    <footer className="p-4 sm:p-8">
 
     </footer>
   );
--- components/Hero.js
+++ components/Hero.js
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default function Hero() {
   return (
-    <div>Hero</div>
+    <div className="py-10 sm:py-14 md:py-20">
+      <h1><span>Moodly</span> helps you track your <span>daily</span> mood!</h1>
+    </div>
   )
 }
