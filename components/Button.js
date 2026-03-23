c97671c102f2819180b7c99234ca01cc6db02f45
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-24 00:03:05 +0200

    Create the button component

---
+++ components/Button.js
@@ -0,0 +1,23 @@
+import { Fugaz_One } from "next/font/google";
+import React from "react";
+
+const fugazOne = Fugaz_One({
+  variable: "--font-fugaz-one",
+  subsets: ["latin"],
+  weight: ["400"],
+});
+
+export default function Button(props) {
+  const { text, dark } = props;
+
+  return (
+    <button
+      className={
+        "rounded-full overflow-hidden duration-200 hover:opacity-60 border-2 border-solid border-indgo-600 " +
+        (dark ? " text-white bg-indigo-600" : " text-indigo-600 ")
+      }
+    >
+      <p className={ "px-6 sm:px-10 whitespace-nowrap py-2 sm:py-3 " + fugazOne.className}>{text}</p>
+    </button>
+  );
+}
