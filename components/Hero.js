72d8198560d069202c1d0db83e004bdd9ac5f270
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-23 23:38:22 +0200

    Change the h1 styling of the hero section

--- components/Hero.js
+++ components/Hero.js
@@ -1,18 +1,21 @@
-import { Fugaz_One } from 'next/font/google';
-import React from 'react'
-
+import { Fugaz_One } from "next/font/google";
+import React from "react";
 
 const fugazOne = Fugaz_One({
   variable: "--font-fugaz-one",
   subsets: ["latin"],
-  weight: ["400"]
+  weight: ["400"],
 });
-
 
 export default function Hero() {
   return (
     <div className="py-10 sm:py-14 md:py-20">
-      <h1 className={`${fugazOne.className}`} ><span>Moodly</span> helps you track your <span>daily</span> mood!</h1>
+      <h1
+        className={`text-5xl sm:text-text-6xl md:text-7xl text-center ${fugazOne.className}`}
+      >
+        <span className="textGradient">Moodly</span> helps you track your{" "}
+        <span className="textGradient">daily</span> mood!
+      </h1>
     </div>
-  )
+  );
 }
