d735036120c00502e6c39a47543794ce33f63875
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-24 11:01:08 +0200

    Add the moods buttons and style them

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -9,7 +9,6 @@ const fugazOne = Fugaz_One({
 });
 
 export default function Dashboard() {
-
   const statuses = {
     num_days: 14,
     time_remaining: "13:14:26",
@@ -18,12 +17,11 @@ export default function Dashboard() {
 
   const moods = {
     "@#$%!": "🤬",
-    "Sad": "😔",
-    "Existing": "😶",
-    "Good": "😊",
-    "Elated": "😍",
+    Sad: "😔",
+    Existing: "😶",
+    Good: "😊",
+    Elated: "😍",
   };
-
 
   return (
     <div className="flex flex-col flex-1 gap-6 sm:gap-12 md:gap-16">
@@ -44,17 +42,26 @@ export default function Dashboard() {
           );
         })}
       </div>
-      <h4 className={`text-5xl sm:text-6xl md:text-7xl text-center ${fugazOne.className}`}>
+      <h4
+        className={`text-5xl sm:text-6xl md:text-7xl text-center ${fugazOne.className}`}
+      >
         How do you <span className="textGradient">feel</span> today?
       </h4>
-      <div className="grid grid-cols-2 md:grid-col-5 gap-4">
+      <div className="flex items-stretch flex-wrap gap-4">
         {Object.keys(moods).map((mood, moodIndex) => {
           return (
-            <button className={`${moodIndex === 4 ? "col-span-2": ""}`} key={moodIndex}>
-              <p>{mood}</p>
-              <p>{moods[mood]}</p>
+            <button
+              className="p-4 px-5 rounded-2xl purpleShadow duration-200 bg-indigo-50 hover:bg-indigo-100 text-center flex flex-col items-center gap-2 flex-1"
+              key={moodIndex}
+            >
+              <p className="text-4xl sm:text-5xl md:text-6xl">{moods[mood]}</p>
+              <p
+                className={`text-indigo-500 text-xs sm:text-sm md:text-base ${fugazOne.className}`}
+              >
+                {mood}
+              </p>
             </button>
-          )
+          );
         })}
       </div>
     </div>
