82558376db6367aeeead210f58f16049711d81f3
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-24 10:19:40 +0200

    Create dummy statuses and emojie buttons for dashboard

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,7 +1,62 @@
-import React from 'react'
+import { NEXT_RESUME_HEADER } from "next/dist/lib/constants";
+import { Fugaz_One } from "next/font/google";
+import React from "react";
+
+const fugazOne = Fugaz_One({
+  variable: "--font-fugaz-one",
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function Dashboard() {
+
+  const statuses = {
+    num_days: 14,
+    time_remaining: "13:14:26",
+    date: new Date().toDateString(),
+  };
+
+  const moods = {
+    "@#$%!": "🤬",
+    "Sad": "😔",
+    "Existing": "😶",
+    "Good": "😊",
+    "Elated": "😍",
+  };
+
+
   return (
-    <div>Dashboard</div>
-  )
+    <div className="flex flex-col flex-1 gap-6 sm:gap-12 md:gap-16">
+      <div className="grid grid-cols-3 bg-indigo-50 text-indigo-500 p-4 gap-4 rounded-lg">
+        {Object.keys(statuses).map((status, statusIndex) => {
+          return (
+            <div key={statusIndex} className=" flex flex-col gap-1 sm:gap-2">
+              <p className="font-medium capitalize text-xs sm:text-sm truncate">
+                {status.replaceAll("_", " ")}
+              </p>
+              <p
+                className={`text-base sm:text-lg truncate ${fugazOne.className}`}
+              >
+                {statuses[status]}
+                {status === "num_days" ? " 🔥" : ""}
+              </p>
+            </div>
+          );
+        })}
+      </div>
+      <h4 className={`text-5xl sm:text-6xl md:text-7xl text-center ${fugazOne.className}`}>
+        How do you <span className="textGradient">feel</span> today?
+      </h4>
+      <div className="grid grid-cols-2 md:grid-col-5 gap-4">
+        {Object.keys(moods).map((mood, moodIndex) => {
+          return (
+            <button className={`${moodIndex === 4 ? "col-span-2": ""}`} key={moodIndex}>
+              <p>{mood}</p>
+              <p>{moods[mood]}</p>
+            </button>
+          )
+        })}
+      </div>
+    </div>
+  );
 }
