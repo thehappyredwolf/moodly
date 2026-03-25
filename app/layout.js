b4e69d6a7560ccb2be93b524fc192bcffed44161
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-25 16:32:26 +0200

    Add Logout component to RootLayout for user logout functionality

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,7 @@ import "./globals.css";
 import Link from "next/link";
 import { AuthProvider } from "@/context/AuthContext";
 import Head from "./head";
+import Logout from "@/components/Logout";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -38,6 +39,7 @@ export default function RootLayout({ children }) {
           Moodly
         </h1>
       </Link>
+      <Logout />
     </header>
   );
 
