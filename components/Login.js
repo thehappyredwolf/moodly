a37206b0d713f488560d6c2b7f61fc679e381c2c
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-25 15:37:45 +0200

    Implement login and registration functionality in Login component

--- components/Login.js
+++ components/Login.js
@@ -1,6 +1,9 @@
+"use client";
+
 import { Fugaz_One } from "next/font/google";
-import React from "react";
+import React, { useState } from "react";
 import Button from "./Button";
+import { useAuth } from "@/context/AuthContext";
 
 const fugazOne = Fugaz_One({
   variable: "--font-fugaz-one",
@@ -9,28 +12,74 @@ const fugazOne = Fugaz_One({
 });
 
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [isRegister, setIsRegister] = useState(false);
+  const [authenticating, setAuthenticating] = useState(false);
+  const { signup, login } = useAuth();
+
+  async function handleSubmit() {
+    if (!email || !password || password.length < 6) {
+      return;
+    }
+
+    setAuthenticating(true);
+
+    try {
+      if (isRegister) {
+        await signup(email, password);
+      } else {
+        await login(email, password);
+      }
+    } catch (err) {
+      console.log(err.message);
+    } finally {
+      setAuthenticating(false);
+    }
+  }
+
   return (
     <div className="flex flex-col flex-1 justify-center items-center gap-4">
       <h3 className={`text-4xl sm:text-5xl md:text-6xl ${fugazOne.className}`}>
-        Log In / Register
+        {isRegister ? "Register" : "Log In"}
       </h3>
       <p>You are one step away!</p>
       <input
+        value={email}
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
         className="max-w-[400px] w-full mx-auto px-3 py-2 duration-200 hover:border-indigo-600
        sm:py-3 border border-solid border-indigo-400 rounded-full outline-none focus:border-indigo-600"
         placeholder="Email"
         type="email"
       />
       <input
+        value={password}
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
         className="max-w-[400px] w-full mx-auto px-3 py-2 duration-200 hover:border-indigo-600
        sm:py-3 border border-solid border-indigo-400 rounded-full outline-none focus:border-indigo-600"
         placeholder="Password"
         type="password"
       />
       <div className="max-w-[400px] w-full mx-auto">
-        <Button text="Submit" full />
+        <Button
+          clickHandler={handleSubmit}
+          text={authenticating ? "Submitting" : "Submit"}
+          full
+        />
       </div>
-      <p className="text-center">Do not have an account? <span className="text-indigo-600">Sign Up</span></p>
+      <p className="text-center">
+        {isRegister ? "Already have an account? " : "Do not have an account? "}
+        <button
+          onClick={() => setIsRegister(!isRegister)}
+          className="text-indigo-600"
+        >
+          {isRegister ? "Sign In" : "Sign Up"}
+        </button>
+      </p>
     </div>
   );
 }
