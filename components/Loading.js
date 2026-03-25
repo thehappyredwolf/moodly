bd9cb7767d5435d054e9794de6fb51c64b8357c9
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-25 08:07:41 +0200

    Update Loading component to include text color for spinner icon

--- components/Loading.js
+++ components/Loading.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Loading() {
   return (
     <div className="flex flex-col flex-1 justify-center items-center">
-      <i className="fa-solid fa-spinner animate-spin text-4xl sm:text-5xl"></i>
+      <i className="fa-solid text-slate-800 fa-spinner animate-spin text-4xl sm:text-5xl"></i>
     </div>
   );
 }
