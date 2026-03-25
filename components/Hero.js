cf286d26e64d92c1d0f3cadf64b887d04a0144f8
thehappyredwolf <228173249+thehappyredwolf@users.noreply.github.com>
2026-03-25 16:27:15 +0200

    Add CallToAction component and integrate it into Hero component

--- components/Hero.js
+++ components/Hero.js
@@ -3,6 +3,7 @@ import React from "react";
 import Button from "./Button";
 import Calendar from "./Calendar";
 import Link from "next/link";
+import CallToAction from "./CallToAction";
 
 const fugazOne = Fugaz_One({
   variable: "--font-fugaz-one",
@@ -23,14 +24,7 @@ export default function Hero() {
         Create your mood record and see how you feel on{" "}
         <span className="font-bold"> every day of every year.</span>
       </p>
-      <div className="grid grid-cols-2 gap-4 w-fit mx-auto">
-        <Link href="/dashboard">
-          <Button text="Sign Up" />
-        </Link>
-        <Link href="/dashboard">
-          <Button text="Login" dark />
-        </Link>
-      </div>
+      <CallToAction />
       <Calendar demo />
     </div>
   );
